b0652c7a7f90f18dfac7315048c87d18ae7abf88
Tun-Sun <itpmcikz@gmail.com>
2026-07-24 16:47:26 +0500

    fix: add --force to seed_data in start.sh so users are created in production

--- start.sh
+++ start.sh
@@ -3,6 +3,6 @@ set -e
 cd backend
 python manage.py migrate
 python manage.py import_json --skip-existing
-python manage.py seed_data --keep-subjects
+python manage.py seed_data --keep-subjects --force
 python manage.py ensure_admin
 exec gunicorn core.wsgi:application --bind 0.0.0.0:$PORT
